3cbdfb9e9071fdc1ba1d2cffb094448c6ba066e4
EarlClaro <earlclaro@gmail.com>
2023-12-16 15:06:24 +0800

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def load_user(user_id):
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Moplex-storm123",
+    password="July82001Cl@ro",
     database="notesdb"
 )
 
